95e0e35be5b9274b117627e54fd8908b37a1c325
eleander <edward.leander@hotmail.com>
2023-08-02 20:53:42 +0200

    summary finished

---
+++ components/summary/SummaryPresenter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <SummaryView
+      :shoppingList="shoppingList"
+      :menuPrice="menuPriceCalculated"
+      :dishes="store.dishes"
+      :number="store.numberOfGuests"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useMyDinnerStore } from "@/stores/dinner";
+
+const store = useMyDinnerStore();
+
+const shoppingList = computed(() => {
+  return useShoppingList(store.dishes);
+});
+
+const menuPriceCalculated = computed(() => {
+  return (menuPrice(store.dishes) * store.numberOfGuests).toFixed(2);
+});
+</script>
+
+<style></style>
---
+++ components/summary/SummaryView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <table class="table">
+      <tbody>
+        <tr>
+          <th>Dish Name</th>
+          <th>Type</th>
+          <th>Cost</th>
+        </tr>
+        <tr v-for="dish in dishes" :key="dish.id">
+          <td>
+            <a>{{ dish.title }}</a>
+          </td>
+          <td>{{ dish.dishTypes[0] }}</td>
+          <td class="dishPrice">
+            {{ (dish.pricePerServing * number).toFixed(2) }}
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Total:</td>
+          <td></td>
+          <td class="dishPrice">
+            {{ menuPrice }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ExtendedIngredient, ReducedDishDetails } from "types";
+defineProps<{
+  shoppingList: ExtendedIngredient[];
+  number: number;
+  menuPrice: string;
+  dishes: ReducedDishDetails[];
+}>();
+</script>
+
+<style></style>
