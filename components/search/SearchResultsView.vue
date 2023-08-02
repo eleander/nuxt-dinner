1b42f0aa106864e4fbc060a6f66c2945d8ec7a9f
eleander <edward.leander@hotmail.com>
2023-08-02 18:30:26 +0200

    lazy load imaage and client only

--- components/search/SearchResultsView.vue
+++ components/search/SearchResultsView.vue
@@ -5,13 +5,15 @@
       @click="$emit('selectDish', dish.id)"
       class="card bg-base-100 shadow-xl w-64 m-3"
     >
-      <NuxtImg
-        loading="lazy"
-        width="256"
-        height="150"
-        :src="dish.image"
-        :alt="dish.title"
-      />
+      <ClientOnly>
+        <NuxtImg
+          loading="lazy"
+          width="256"
+          height="150"
+          :src="dish.image"
+          :alt="dish.title"
+        />
+      </ClientOnly>
       <div class="card-body">
         <h1 class="card-title">{{ dish.title }}</h1>
       </div>
