a0842635cfd73e0cb957e25365dff9be02851ad8
eleander <edward.leander@hotmail.com>
2023-08-02 23:18:36 +0200

    SEO

--- app.vue
+++ app.vue
@@ -5,3 +5,25 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+const description =
+  "This is an app to help you plan your dinner. It is built with Nuxt.js and TailwindCSS. It is used as a teaching tool in DH2643";
+
+useHead({
+  title: "Dinner Planner",
+  meta: [
+    {
+      name: description,
+    },
+  ],
+});
+
+useServerSeoMeta({
+  title: "Dinner Planner",
+  description: description,
+  ogTitle: "Dinner Planner for DH2643",
+  ogDescription: description,
+  ogImage: "/favicon.ico",
+});
+</script>
