31eec5c02fea1ba6978ab1d71e1a90bedd92fbb7
eleander <edward.leander@hotmail.com>
2023-08-02 17:42:10 +0200

    tailwindcss/typography is OP

--- components/details/DetailsView.vue
+++ components/details/DetailsView.vue
@@ -1,19 +1,24 @@
 b
 <template>
   <div>
-    <div v-if="dish">
-      <button :disabled="isDishInMenu" @click="$emit('addToMenu')">
+    <div v-if="dish" class="prose">
+      <h1>{{ dish.title }}</h1>
+      <button
+        :disabled="isDishInMenu"
+        @click="$emit('addToMenu')"
+        class="btn btn-ghost"
+      >
         Add to Menu
       </button>
-      <button @click="$emit('cancel')">Cancel</button>
-      <h1>{{ dish.title }}</h1>
-      <div>
-        {{ dish.pricePerServing }}. For {{ guests }}:
-        {{ (dish.pricePerServing * guests).toFixed(2) }}
-      </div>
-      <NuxtImg :src="dish.image" height="300" />
-      <div>{{ dish.instructions }}</div>
-      <div><a :href="dish.sourceUrl">Original recipe</a></div>
+      <button @click="$emit('cancel')" class="btn btn-ghost">Cancel</button>
+      <p>Price per servings:{{ dish.pricePerServing }}$</p>
+      <p>
+        For {{ guests }} guests:
+        {{ (dish.pricePerServing * guests).toFixed(2) }}$
+      </p>
+      <NuxtImg :src="dish.image" class="rounded-lg" />
+      <p>{{ dish.instructions }}</p>
+      <p><a :href="dish.sourceUrl">Original recipe</a></p>
       <ol>
         <li v-for="ingr in dish.extendedIngredients" :key="ingr.id">
           {{ ingr.name }} {{ ingr.amount }} {{ ingr.unit }}
